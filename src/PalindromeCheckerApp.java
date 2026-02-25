503929d478ed3ffdcf685b629560f5e4673b5061
Victorralph7011 <victorralph7011@gmail.com>
2026-02-25 15:34:49 +0530

    UC3: Implement palindrome check using manual string reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,26 +1,20 @@
-/**
- * UC2: Print a Hardcoded Palindrome Result
- * Goal: Display whether a hardcoded string is a palindrome.
- */
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        // UC1: Welcome Message
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
 
-        // UC2: Hardcoded String Logic
-        String input = "madam"; // Hardcoded string literal
+
+        String input = "madam";
         String reversed = "";
 
-        // Core programming logic to reverse the string
+        // Iterate from the last character to the first
         for (int i = input.length() - 1; i >= 0; i--) {
             reversed = reversed + input.charAt(i);
         }
 
-        System.out.println("\n--- UC2: Hardcoded Check ---");
-        System.out.println("Input String: " + input);
+        System.out.println("\nInput String: " + input);
+        System.out.println("Reversed String: " + reversed);
 
-        // Conditional Statement (if-else) to validate palindrome
         if (input.equalsIgnoreCase(reversed)) {
             System.out.println("Result: The string is a PALINDROME.");
         } else {
